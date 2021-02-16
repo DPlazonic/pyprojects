44ffea76183c3443a145762046d3f0c337964c05
DPlazonic <66380253+DPlazonic@users.noreply.github.com>
2021-02-16 21:13:54 +0100

    Update Mood Checker.py

---
+++ Mood Checker.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood?")
+
+if mood == "happy":
+    print("It is great to see you happy")
+elif mood == "nervous":
+    print("Take adeep breath 3 times")
+elif mood == "sad":
+    print("Go work out or take walk")
+elif mood == "excited":
+    print("Drink some water")
+elif mood == "relaxed":
+    print("Close your eyes and sleep")
+else:
+    print("I don't recognize this mood")
