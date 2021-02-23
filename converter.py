d22765e04a3f274304fba4b5380f5ad7fa9024f0
DPlazonic <66380253+DPlazonic@users.noreply.github.com>
2021-02-23 10:50:32 +0100

    first

---
+++ converter.py
@@ -0,0 +1,8 @@
+print("Hey! This is unit converter, you can convert kilometers into miles")
+
+while True:
+   km = float(input("Enter value in km: "))
+
+   miles = km * 0.621371
+
+   print(str(km)+ " " + "kilometers is" + str(miles)+ " " + "miles")
